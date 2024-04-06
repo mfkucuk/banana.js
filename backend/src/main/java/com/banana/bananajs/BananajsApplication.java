47be673a36ec70d80082f402d7f225113ad056f6
Mehmet Feyyaz Kucuk <mfkucuk555@gmail.com>
2024-04-06 15:59:17 +0300

    feat: spring boot init

---
+++ backend/src/main/java/com/banana/bananajs/BananajsApplication.java
@@ -0,0 +1,13 @@
+package com.banana.bananajs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BananajsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BananajsApplication.class, args);
+	}
+
+}
